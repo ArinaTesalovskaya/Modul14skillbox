f1b039461330341dc165d2892e9932342712051c
Arina <tesalovskaya.arina@yandex.ru>
2022-03-14 17:00:13 +0300

    Initial Commit

---
+++ Mod14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mod14
+//
+//  Created by Arina Tesalovskaya on 14.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
